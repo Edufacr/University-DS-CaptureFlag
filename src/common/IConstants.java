9a941da55fa81c55f96d8a9425111a7d13cf255c
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-13 20:29:23 -0600

    Added grid and cell values

--- src/common/IConstants.java
+++ src/common/IConstants.java
@@ -3,4 +3,8 @@ package common;
 public interface IConstants {
 	public static final int WINDOW_WIDTH = 1024;
 	public static final int WINDOW_HEIGHT = 800;
+	public static final int GRID_HEIGHT = 30;
+	public static final int GRID_WIDTH = 30;
+	public static final int CELL_WIDTH = 42;
+	public static final int CELL_HEIGHT = 30;
 }
