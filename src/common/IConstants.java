c55976b60a3cae862f381b1a890deab49c118003
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-13 21:11:57 -0600

    Worked on cell size

--- src/common/IConstants.java
+++ src/common/IConstants.java
@@ -3,8 +3,10 @@ package common;
 public interface IConstants {
 	public static final int WINDOW_WIDTH = 1024;
 	public static final int WINDOW_HEIGHT = 800;
-	public static final int GRID_HEIGHT = 30;
-	public static final int GRID_WIDTH = 30;
-	public static final int CELL_WIDTH = 42;
-	public static final int CELL_HEIGHT = 30;
+	public static final int GRID_HEIGHT = 25;
+	public static final int GRID_WIDTH = 32;
+	public static final int CELL_WIDTH = 32;
+	public static final int CELL_HEIGHT = 32;
+	
+	public static final String WINDOW_NAME = "Capture the Flag";
 }
--- src/gui/mainWindow.java
+++ src/gui/mainWindow.java
@@ -1,54 +1,42 @@
 package gui;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.util.Hashtable;
 import common.IConstants;
 
 public class mainWindow implements IConstants{
 	
 	private JFrame frame;
-	private Hashtable<int[], JPanel> cells;
+	private JPanel[][] cells;
 	
 	public mainWindow() {
-		this.cells = new Hashtable<int[], JPanel>();
-		this.frame =  new JFrame("Main");
-		frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(30, 30));
-		this.fillGrid();
+		this.cells = new JPanel[GRID_WIDTH][GRID_HEIGHT];
 		
-		frame.setVisible(true);
+		this.frame =  new JFrame(WINDOW_NAME);
+		this.frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		this.frame.setResizable(false);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setLayout(new GridLayout(GRID_WIDTH, GRID_HEIGHT));
+		this.fillGrid();
+		this.frame.setVisible(true);
 	}
 	
 	public void fillGrid() {
-		int[] key;;
 		JPanel current;
-		for (int i = 0; i < 30; i++) {
-			for (int j = 0; i < 30; i++) {
-				key = new int[] {i,j};
-				cells.put(key, new JPanel());
-				current = cells.get(key);
-				current.setSize(30, 20);
+		for (int i = 0; i < GRID_WIDTH; i++) {
+			for (int j = 0; j < GRID_HEIGHT; j++) {
+				cells[i][j] =  new JPanel();
+				current = cells[i][j];
+				current.setSize(CELL_WIDTH, CELL_HEIGHT);
 				
-				if (i%2==0) {
-					if (j%2==0) {
-						current.setBackground(Color.blue);
-					} else {current.setBackground(Color.green);}
-				} else {
-					if (j%2==0) {
-						current.setBackground(Color.green);
-					} else {current.setBackground(Color.blue);}
-				}
+				if ((i+j)%2!=0) {
+					current.setBackground(Color.blue);
+				} else {current.setBackground(Color.green);}
 				
 				this.frame.add(current);
-				current.setVisible(true);
 			}
 		}
 	}
