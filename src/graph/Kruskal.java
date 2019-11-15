c381081d9d2a3f0958b5923566f4151b7d048cd5
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-14 21:53:38 -0600

    Created Kruskal class

---
+++ src/graph/Kruskal.java
@@ -0,0 +1,24 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Kruskal <T>{
+	private ArrayList<GraphNode<T>> path;
+	private ArrayList<GraphNode<T>> nodes;
+	//private ArrayList<Edge<GraphNode<T>>> edges;
+	
+	public ArrayList<GraphNode<T>> getPath(Graph<T> pGraph, T pValue1, T pValue2) {
+		this.calculateMST(pGraph);
+		return null;
+	}
+	
+	private void calculateMST(Graph<T> pGraph) {
+		this.buildEdges(pGraph);
+		return;
+	}
+	
+	private void buildEdges(Graph<T> pGraph) {
+		//Collections.sort(this.edges);
+		return;
+	}
+}
