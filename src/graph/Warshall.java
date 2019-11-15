cd3ff3870a4b162e3a91c5338a2fd75ff2f374bf
Eduardo <edumadrim@gmail.com>
2019-11-15 01:07:44 -0600

    Warshall Algorithm

--- src/graph/Warshall.java
+++ src/graph/Warshall.java
@@ -1,17 +1,21 @@
 package graph;
 
+import java.util.ArrayList;
+
 public class Warshall<T> {
+    private Graph<T> original;
     private Graph<T> copy;
     public Warshall(){
         copy = null;
+        original = null;
     }
     public void calculateWarshall(Graph<T> pGraph){
-        copy = duplicateGraph(pGraph);
-        for (GraphNode<T> linkNode:pGraph.getNodes()
+        initValues(pGraph);
+        for (GraphNode<T> linkNode:copy.getNodes()
              ) {
-            for (GraphNode<T> sourceNode:pGraph.getNodes()
+            for (GraphNode<T> sourceNode:copy.getNodes()
             ) {
-                for (GraphNode<T> endNode:pGraph.getNodes()
+                for (GraphNode<T> endNode:copy.getNodes()
                 ) {
                     if (copy.areAdjacent(sourceNode,endNode)){
                         continue;
@@ -24,35 +28,80 @@ public class Warshall<T> {
         }
         copy.print();
     }
-    private void addConnection(GraphNode<T> pSourceNode, GraphNode<T> pEndNode,GraphNode<T> pLinkNode){
-        GraphNode<T> newNode = new GraphNode<T>(pEndNode.getContents());
-        newNode.setLast(pLinkNode);
-        pSourceNode.addEdge(newNode,0);
+    private void initValues(Graph<T> pGraph){
+        original = pGraph;
+        copy = duplicateGraph(pGraph);
     }
-    private Graph<T> duplicateGraph(Graph<T> pGraph){
-        return null;
-        /*Graph<T> graph = new Graph<T>();
-        for (GraphNode<T> node:pGraph.getNodes()
+    private ArrayList<GraphNode<T>> getPath(T startContent, T endContent){
+        ArrayList<GraphNode<T>> list = new ArrayList<GraphNode<T>>();
+        GraphNode<T> startNode = copy.getNode(startContent);
+        for (GraphNode<T> node:startNode.getAdjacentNodes()
              ) {
-            graph.addNode(node.getContents());
-        }
-
-            for (GraphNode<T> adjacent:node.getAdjacentNodes()
-                 ) {
-                graph.addEdge(node.getContents(),);
+            if(node.getContents().equals(endContent)){
+                getOriginalNodePath(getPath(node), list);
             }
         }
-        graph.add
-        return null;*/
+        return list;
+    }
+    private ArrayList<GraphNode<T>> getPath(GraphNode<T> pNode){
+        ArrayList<GraphNode<T>> list = new ArrayList<GraphNode<T>>();
+        while(pNode != null){
+            System.out.println(pNode);
+            list.add(0,pNode);
+            pNode = pNode.getLast();
+        }
+        return list;
+    }
+    private void getOriginalNodePath(ArrayList<GraphNode<T>> pCopyPath,ArrayList<GraphNode<T>> pRetPath){
+        for (GraphNode<T> copyPathNode:pCopyPath
+             ) {
+            pRetPath.add(original.getNode(copyPathNode.getContents()));
+        }
+    }
+    private void addConnection(GraphNode<T> pSourceNode, GraphNode<T> pEndNode,GraphNode<T> pLinkNode){
+        GraphNode<T> newNode = new GraphNode<T>(pEndNode.getContents());
+        newNode.setLast(getSourceLinkNodeCopy(pSourceNode,pLinkNode));
+        pSourceNode.addEdge(newNode,0);
+    }
+    private GraphNode<T>getSourceLinkNodeCopy(GraphNode<T> pSourceNode,GraphNode<T> pLinkNode){
+        for (GraphNode<T> node:pSourceNode.getAdjacentNodes()
+             ) {
+            if(pLinkNode.equals(node)){
+                return node;
+            }
+        }
+        return null;
+    }
+    private Graph<T> duplicateGraph(Graph<T> pGraph){
+        Graph<T> newGraph = new Graph<T>();
+        GraphNode<T> newGraphNode;
+        for (GraphNode<T> node:pGraph.getNodes()
+             ) {
+            newGraphNode = new GraphNode<T>(node.getContents());
+            newGraph.addNode(newGraphNode);
+            for (GraphNode<T> adjacent:node.getAdjacentNodes()
+                 ) {
+                newGraphNode.addEdge(new GraphNode<T>(adjacent.getContents()),0);
+            }
+        }
+        return newGraph;
     }
 
     public static void main(String[] args) {
         Graph<Integer> g1 = new Graph<Integer>();
+        Warshall<Integer> w = new Warshall<Integer>();
         g1.addNode(1);
         g1.addNode(2);
         g1.addNode(3);
+        g1.addNode(4);
+        g1.addNode(5);
+        g1.addNode(6);
         g1.addEdge(1,2,0);
         g1.addEdge(2,3,0);
-        g1.addEdge(3,2,0);
+        g1.addEdge(6,4,0);
+        g1.addEdge(6,3,0);
+        g1.print();
+        w.calculateWarshall(g1);
+        System.out.println(w.getPath(1,4).toString());
     }
 }
