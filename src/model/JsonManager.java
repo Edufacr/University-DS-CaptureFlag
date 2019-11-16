18b98f07f91274e775005c45706d5f95f6266cb8
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-15 19:44:08 -0600

    Created JsonManager

---
+++ src/model/JsonManager.java
@@ -0,0 +1,11 @@
+package model;
+
+import common.IConstants;
+
+public class JsonManager implements IConstants{
+	private String filePath;
+	
+	public JsonManager() {
+		this.filePath = JSON_PATH;
+	}
+}
