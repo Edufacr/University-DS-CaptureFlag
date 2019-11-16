e5a271995c32efd89c2fffefe05e729b19091d52
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-15 23:19:06 -0600

    Created characters + basic character functions

---
+++ src/model/characters/Archer.java
@@ -0,0 +1,9 @@
+package model.characters;
+
+public class Archer extends Character{
+	public Archer() {
+		super();
+		super.attackPower = ARCHER_ATTACK_POWER;
+		super.attackTime = ARCHER_ATTACK_TIME;
+	}
+}
