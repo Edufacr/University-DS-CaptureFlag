ce21a572b3873e083a421bb311d19aab234480f1
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-11-15 23:18:34 -0600

    Added character constants

--- src/common/IConstants.java
+++ src/common/IConstants.java
@@ -16,4 +16,13 @@ public interface IConstants {
 	public static final String SECOND_X = "x2";
 	public static final String FIRST_Y = "y1";
 	public static final String SECOND_Y = "y2";
+	
+	public static final int CHARACTER_HP = 100;
+	
+	public static final int MARINE_ATTACK_TIME = 2000;
+	public static final int MARINE_ATTACK_POWER = 80;
+	public static final int PUNCHER_ATTACK_TIME = 1000;
+	public static final int PUNCHER_ATTACK_POWER = 20;
+	public static final int ARCHER_ATTACK_TIME = 1500;
+	public static final int ARCHER_ATTACK_POWER = 40;
 }
