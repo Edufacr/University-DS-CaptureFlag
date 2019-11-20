886b0ace0e8ddb59a067fc6b66cc0da7731c810b
Eduardo <edumadrim@gmail.com>
2019-11-19 22:53:53 -0600

    PathAnalyzer added
    Class for checking if map is solvable

---
+++ src/model/analyzer/PathAnalyzer.java
@@ -0,0 +1,43 @@
+package model.analyzer;
+
+import model.graph.Graph;
+import model.graph.GraphNode;
+import model.graph.Warshall;
+
+import java.util.ArrayList;
+
+public class PathAnalyzer<T> {
+    private Warshall<T> warshall;
+
+    public PathAnalyzer(){
+        warshall = new Warshall<T>(); //*** Hay que cambiar Warshall a Singleton
+    }
+
+    public boolean analyzeGraph(Graph<T> pGraph,ArrayList<T> pPrimaryConnectionsContent){
+        warshall.calculateWarshall(pGraph);
+        Graph<T> graph = warshall.getConnectionGraph();
+        ArrayList<GraphNode<T>> primaryConnections = generatePrimaryConnections(pPrimaryConnectionsContent, graph);
+        for (GraphNode<T> startNode: primaryConnections
+             ) {
+            for (GraphNode<T> endNode: primaryConnections
+            ) {
+                if(!(startNode.equals(endNode)||graph.areAdjacent(startNode,endNode))){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    private ArrayList<GraphNode<T>> generatePrimaryConnections(ArrayList<T> pPrimaryConnectionsContent,Graph<T> pGraph){
+        ArrayList<GraphNode<T>> retList = new ArrayList<>();
+        for (T nodeContent:pPrimaryConnectionsContent
+             ) {
+            retList.add(pGraph.getNode(nodeContent)); //***Se puede poner una condicion de if null
+        }
+        return retList;
+    }
+
+
+}
