514f24543439a08e41f5c76025d973b505c93557
Edufacr <eduardgugu99>
2019-11-20 16:58:38 -0600

    node creation complete

--- src/common/GameManager.java
+++ src/common/GameManager.java
@@ -4,9 +4,13 @@ import model.Square;
 import model.analyzer.ObstacleAnalyzer;
 import model.analyzer.PathAnalyzer;
 import model.graph.Graph;
+import model.graph.GraphNode;
+
+import java.util.ArrayList;
 
 
 public class GameManager implements IConstants {
+    private final int edgeWeight = 1;
     private Graph<Square> graph;
     private PathAnalyzer<Square> pathAnalyzer;
     private ObstacleAnalyzer obstacleAnalyzer;
@@ -32,12 +36,35 @@ public class GameManager implements IConstants {
     }
 
     private void linkGraphNodes(){
-        Graph<Square> actualNode;
-        Graph<Square> nextNode;
-        Graph<Square> belowNode;
-        Graph<Square> diagonalNode;
         for (int nodeNum = 0; nodeNum < graph.getNodes().size() ;nodeNum++){
-
+            linkGraphNode(nodeNum);
         }
     }
+    private void linkGraphNode(int pNodeNum){
+        //TODO Change graph get() to ingore index = -1
+        //TODO Change graph add Edge to ignore null
+        GraphNode<Square> node = graph.getNode(pNodeNum);
+        for (GraphNode<Square> linkingNode: getNodesToLink(pNodeNum)
+             ) {
+            graph.addEdge(node,linkingNode,edgeWeight);
+        }
+
+    }
+    private ArrayList<GraphNode<Square>> getNodesToLink(int pNodeNum){
+        ArrayList<GraphNode<Square>> retList = new ArrayList<>(3);
+        retList.add(getBelowNode(pNodeNum));
+        retList.add(getDiagonalNode(pNodeNum));
+        retList.add(getNextNode(pNodeNum));
+        return retList;
+    }
+    private GraphNode<Square> getDiagonalNode(int pNodeNum){
+        return graph.getNode(pNodeNum+GRID_WIDTH+1);
+    }
+    private GraphNode<Square> getNextNode(int pNodeNum){
+        return graph.getNode(pNodeNum+1);
+    }
+    private GraphNode<Square> getBelowNode(int pNodeNum){
+        return  graph.getNode(pNodeNum+GRID_WIDTH);
+    }
+
 }
