156b9566ec164f6d06cff4bf93d3bfa441391192
Eduardo <edumadrim@gmail.com>
2019-11-21 10:29:45 -0600

    Add setPathGetter, properties, getNextMove

--- src/model/characters/Team.java
+++ src/model/characters/Team.java
@@ -1,20 +1,42 @@
 package model.characters;
 
+import common.GameManager;
 import model.Square;
+import model.graph.GraphNode;
 import model.graph.IGraphPathGettable;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Team {
-    IGraphPathGettable<Square> pathGetter;
-    ArrayList<Character> members;
+    private IGraphPathGettable<Square> pathGetter;
+    private ArrayList<Character> members;
+    private ArrayList<GraphNode<Square>> path;
+    private int actualSquareNum;
 
     public Team(IGraphPathGettable<Square> pPathGetter){
         members = new ArrayList<Character>();
         pathGetter = pPathGetter;
+        path = null;
+        actualSquareNum =0;
     }
+    public Team(ArrayList<Character> pCharacters,int pActualSquareNum){
+        members = pCharacters;
+        pathGetter = null;
+        path = null;
+        actualSquareNum =pActualSquareNum;
+    }
+    public GraphNode<Square> getNextMove(){
+        try{
+            GraphNode<Square> next = path.remove(0);
+            actualSquareNum = next.getContents().getId();
+            return next;
+        }
+        catch (Exception e){
+            return null;
+        }
 
+    }
     public void addMember(Character pCharacter){
         members.add(pCharacter);
     }
@@ -39,4 +61,7 @@ public class Team {
         return members.isEmpty();
     }
 
+    public void setPathGetter(IGraphPathGettable<Square> pathGetter) {
+        this.pathGetter = pathGetter;
+    }
 }
